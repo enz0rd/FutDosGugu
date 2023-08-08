0d047b1774f28fbbd2733630c6faabf25f618529
Enzo <enzorossidaltoe@hotmail.com.br>
2023-08-08 20:33:40 -0300

    Update CadastroController.js

--- Controllers/CadastroController.js
+++ Controllers/CadastroController.js
@@ -13,6 +13,21 @@ class CadastroController {
       res.render("../views/error", { data: error_message });
     }
   }
+
+  static async getLogin() {
+    try {
+      res.sendFile(path.join(__dirname, "../views", "login.html"));
+    } catch (error) {
+      const error_message = [];
+      error_message.push({
+        title: "Error",
+        message: error.message,
+      });
+      res.render("../views/error", { data: error_message });
+    }
+  }
+
+  static async tryLogin(req, res) {}
 }
 
 module.exports = CadastroController;
