cb3044dd5461a69846ba87f308eed886c58f905f
Enzo Rossi Daltoé <enzorossidaltoe@hotmail.com.br>
2023-08-22 18:51:00 -0300

    update v0.00.005
    Initial search methods done

--- Controllers/SearchController.js
+++ Controllers/SearchController.js
@@ -2,88 +2,138 @@ const path = require("path");
 const db = require("../models");
 
 class SearchController {
-    // render page
-    static async getBusca(req,res) {
-        try {
-            let data = [{
-                message: "Comece a pesquisar!"
-            }]
-            res.render("../views/search", { data: data });
-        } catch (error) {
-            console.log(`Erro ao listar: ${error.message}`);
-            const error_message = [];
-            error_message.push({
-                title: "Falha no carregamento",
-                message: error.message,
-            });
-            res.render("../views/error", { data: error_message });
-        }
+  // render page
+  static async getBusca(req, res) {
+    try {
+      let data = [
+        {
+          message: "Comece a pesquisar!",
+        },
+      ];
+      res.render("../views/search", { data: data });
+    } catch (error) {
+      console.log(`Erro ao listar: ${error.message}`);
+      const error_message = [];
+      error_message.push({
+        title: "Falha no carregamento",
+        message: error.message,
+      });
+      res.render("../views/error", { data: error_message });
     }
+  }
 
-    //get user by name
-    static async getUserByName(req, res) {
-        let nome = req.body.query;
-        console.log(req.body)
-        var query = `SELECT 
+  //get user by name
+  static async getUserByName(req, res) {
+    let nome = req.body.query;
+    console.log(req.body);
+    var query = `SELECT 
         u.id, 
         u.nome, 
         u.biografia, 
         p.nome_posicao, 
         c.nome_cidade, 
         est.nome_estado FROM users u JOIN posicoes p ON u.id_posicao = p.id JOIN cidades c ON u.id_cidade = c.id JOIN estados est ON u.id_estado = est.id WHERE u.nome LIKE '%${nome}%';`;
-        try {
-            let result = await db.sequelize.query(query);
-            if (result[0].length === 0 && result[1].length === 0) {
-                result = [{ message: "Nenhum usuário encontrado" }];
-            }
-            res.status(200).send(result);
-        } catch (error) {
-            console.log(`Erro ao listar: ${error.message}`);
-            const error_message = [];
-            error_message.push({
-                title: "Falha na consulta",
-                message: error.message,
-            });
-            res.render("../views/error", { data: error_message });
-        }
+    try {
+      let result = await db.sequelize.query(query);
+      if (result[0].length === 0 && result[1].length === 0) {
+        result = [{ message: "Nenhum usuário encontrado" }];
+      }
+      res.status(200).send(result);
+    } catch (error) {
+      console.log(`Erro ao listar: ${error.message}`);
+      const error_message = [];
+      error_message.push({
+        title: "Falha na consulta",
+        message: error.message,
+      });
+      res.render("../views/error", { data: error_message });
     }
+  }
 
-    static async getByCidade(req,res) {
-        let nome = req.body.query;
-        console.log(req.body)
-        var query = `SELECT 
+  static async getByEstado(req, res) {
+    let nome = req.body.query;
+    console.log(req.body);
+    var query = `SELECT 
+        id, 
+        nome_estado,
+        sigla
+        FROM estados 
+        WHERE nome_estado LIKE '%${nome}%';`;
+    try {
+      let result = await db.sequelize.query(query);
+      if (result[0].length === 0 && result[1].length === 0) {
+        result = [{ message: "Nenhum estado encontrado" }];
+      }
+      console.log(result);
+      res.status(200).send(result);
+    } catch (error) {
+      console.log(`Erro ao listar: ${error.message}`);
+      const error_message = [];
+      error_message.push({
+        title: "Falha na consulta",
+        message: error.message,
+      });
+      res.render("../views/error", { data: error_message });
+    }
+  }
+
+  static async getByCidade(req, res) {
+    let nome = req.body.query;
+    console.log(req.body);
+    var query = `SELECT 
         c.id, 
         c.nome_cidade, 
         est.nome_estado 
         FROM cidades c 
         JOIN estados est ON c.id_estado = est.id 
         WHERE c.nome_cidade LIKE '%${nome}%';`;
-        try {
-            let result = await db.sequelize.query(query);
-            if (result[0].length === 0 && result[1].length === 0) {
-                result = [{ message: "Nenhuma cidade encontrada" }];
-            }
-            console.log(result)
-            res.status(200).send(result);
-        } catch (error) {
-            console.log(`Erro ao listar: ${error.message}`);
-            const error_message = [];
-            error_message.push({
-                title: "Falha na consulta",
-                message: error.message,
-            });
-            res.render("../views/error", { data: error_message });
-        }
+    try {
+      let result = await db.sequelize.query(query);
+      if (result[0].length === 0 && result[1].length === 0) {
+        result = [{ message: "Nenhuma cidade encontrada" }];
+      }
+      console.log(result);
+      res.status(200).send(result);
+    } catch (error) {
+      console.log(`Erro ao listar: ${error.message}`);
+      const error_message = [];
+      error_message.push({
+        title: "Falha na consulta",
+        message: error.message,
+      });
+      res.render("../views/error", { data: error_message });
     }
-    
-    static async getByEquipe(req,res) {
+  }
 
+  static async getByEquipe(req, res) {
+    let nome = req.body.query;
+    console.log(req.body);
+    var query = `SELECT 
+        e.id, 
+        e.nome_equipe,
+        e.observacoes,
+        COUNT(j.id) AS numero_jogadores
+        FROM equipes e
+        LEFT JOIN jogadores j ON e.id = j.id_equipe
+        WHERE e.nome_equipe LIKE '%${nome}%'
+        GROUP BY e.id, e.nome_equipe, e.observacoes;`;
+    try {
+      let result = await db.sequelize.query(query);
+      if (result[0].length === 0 && result[1].length === 0) {
+        result = [{ message: "Nenhuma equipe encontrada" }];
+      }
+      console.log(result);
+      res.status(200).send(result);
+    } catch (error) {
+      console.log(`Erro ao listar: ${error.message}`);
+      const error_message = [];
+      error_message.push({
+        title: "Falha na consulta",
+        message: error.message,
+      });
+      res.render("../views/error", { data: error_message });
     }
-
-    static async getByEstado(req,res) {
-
-    }
-
+  }
 }
 
-module.exports = SearchController
+module.exports = SearchController;
--- src/js/search.js
+++ src/js/search.js
@@ -4,126 +4,192 @@ const queryInput = document.getElementById("query");
 
 searchButton.addEventListener("click", performSearch);
 queryInput.addEventListener("keydown", function (event) {
-    if (event.key === "Enter") {
-        event.preventDefault(); // Evita o comportamento padrão de enviar o formulário
-        performSearch();
-    }
+  if (event.key === "Enter") {
+    event.preventDefault(); // Evita o comportamento padrão de enviar o formulário
+    performSearch();
+  }
 });
 
 function performSearch() {
-    const dropdownValue = document.getElementById("dropdown").value;
-    const queryValue = queryInput.value;
-    if (dropdownValue != "") {
-        if (queryValue) {
-            var formData = {
-                dropdown: dropdownValue,
-                query: queryValue
-            };
+  const dropdownValue = document.getElementById("dropdown").value;
+  const queryValue = queryInput.value;
+  if (dropdownValue != "") {
+    if (queryValue) {
+      var formData = {
+        dropdown: dropdownValue,
+        query: queryValue,
+      };
 
-            var urlParams = new URLSearchParams(window.location.search)
-            urlParams.set('dropdown', dropdownValue);
-            var newURL = window.location.pathname + '?' + urlParams.toString();
-            window.history.replaceState({}, '', newURL);
+      var urlParams = new URLSearchParams(window.location.search);
+      urlParams.set("dropdown", dropdownValue);
+      var newURL = window.location.pathname + "?" + urlParams.toString();
+      window.history.replaceState({}, "", newURL);
 
-            // Aqui você precisaria usar uma função para enviar os dados para o servidor
-            // Por exemplo, usando a API fetch() ou XMLHttpRequest()
+      // Aqui você precisaria usar uma função para enviar os dados para o servidor
+      // Por exemplo, usando a API fetch() ou XMLHttpRequest()
 
-            // Exemplo usando a API fetch()
-            fetch(window.location.href, {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json"
-                },
-                body: JSON.stringify(formData)
-            })
-                .then(response => response.json())
-                .then(data => {
-                    // Aqui você pode lidar com a resposta do servidor
-                    var data_view = document.getElementById('data');
-                    data_view.innerHTML = "";
-                    if (data[0].message) {
-                        var paragraph = document.createElement('p');
-                        paragraph.textContent = data[0].message
-                        data_view.appendChild(paragraph);
-                    }
+      // Exemplo usando a API fetch()
+      fetch(window.location.href, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          // Aqui você pode lidar com a resposta do servidor
+          var data_view = document.getElementById("data");
+          data_view.innerHTML = "";
+          if (data[0].message) {
+            var paragraph = document.createElement("p");
+            paragraph.textContent = data[0].message;
+            data_view.appendChild(paragraph);
+          }
 
-                    if (dropdownValue == 'user') {
-                        listarUsers(data);
-                    } else if (dropdownValue == 'cidade') {
-                        listarCidades(data)
-                    } else if (dropdownValue == 'estado') {
-                        listarEstados(data)
-                    } else if (dropdownValue == 'equipe') {
-                        listarEquipes(data)
-                    }
-
-                })
-                .catch(error => {
-                    console.error("Ocorreu um erro:", error);
-                });
-        } else {
-            alert("Por favor, preencha o campo de consulta.");
-        }
+          if (dropdownValue == "user") {
+            listarUsers(data);
+          } else if (dropdownValue == "cidade") {
+            listarCidades(data);
+          } else if (dropdownValue == "estado") {
+            listarEstados(data);
+          } else if (dropdownValue == "equipe") {
+            listarEquipes(data);
+          }
+        })
+        .catch((error) => {
+          console.error("Ocorreu um erro:", error);
+        });
     } else {
-        alert("Por favor, preencha o campo de tipo de consulta.");
+      alert("Por favor, preencha o campo de consulta.");
     }
-};
+  } else {
+    alert("Por favor, preencha o campo de tipo de consulta.");
+  }
+}
 
 function listarUsers(data) {
-    var data_view = document.getElementById('data');
-    data_view.innerHTML = ""; // Limpar o conteúdo antes de começar
+  var data_view = document.getElementById("data");
+  data_view.innerHTML = ""; // Limpar o conteúdo antes de começar
 
-    data[0].forEach(function (dado, indice) {
-        var div = document.createElement('div');
-        var paragraph = document.createElement('p');
+  data[0].forEach(function (dado, indice) {
+    var link = document.createElement("a")
+    var div = document.createElement("div");
+    var paragraph = document.createElement("p");
 
-        var nome = document.createElement('h3');
-        var posicao = document.createElement('h4');
-        var cidade = document.createElement('h4');
+    link.setAttribute('href', `/user/${dado.id}`)
 
-        paragraph.textContent = "Dados Usuário #" + (indice + 1) + ": ";
-        nome.textContent = "Nome: " + dado.nome;
-        posicao.textContent = "Posição: " + dado.nome_posicao;
-        cidade.textContent = "Origem: " + dado.nome_cidade + ", " + dado.nome_estado;
+    var nome = document.createElement("h3");
+    var posicao = document.createElement("h4");
+    var cidade = document.createElement("h4");
 
-        div.setAttribute('class', 'user_card')
+    paragraph.textContent = "Dados Usuário #" + (indice + 1) + ": ";
+    nome.textContent = "Nome: " + dado.nome;
+    posicao.textContent = "Posição: " + dado.nome_posicao;
+    cidade.textContent =
+      "Origem: " + dado.nome_cidade + ", " + dado.nome_estado;
 
-        div.appendChild(paragraph);
-        div.appendChild(nome);
-        div.appendChild(posicao);
-        div.appendChild(cidade);
+    div.setAttribute("class", "user_card");
 
-        data_view.appendChild(div);
-    });
+    div.appendChild(paragraph);
+    div.appendChild(nome);
+    div.appendChild(posicao);
+    div.appendChild(cidade);
+
+    link.appendChild(div);
+    data_view.appendChild(link);
+  });
 }
 
 function listarCidades(data) {
-    var data_view = document.getElementById('data');
-    data_view.innerHTML = ""; // Limpar o conteúdo antes de começar
+  var data_view = document.getElementById("data");
+  data_view.innerHTML = ""; // Limpar o conteúdo antes de começar
 
-    data[0].forEach(function (dado, indice) {
-        var link = document.createElement('a');
-        
-        link.setAttribute('href', `/cidade/${dado.id}`)
+  data[0].forEach(function (dado, indice) {
+    var link = document.createElement("a");
 
-        var div = document.createElement('div');
-        var paragraph = document.createElement('p');
+    link.setAttribute("href", `/cidade/${dado.id}`);
 
-        var nome = document.createElement('h3');
-        var estado = document.createElement('h4');
+    var div = document.createElement("div");
+    var paragraph = document.createElement("p");
 
-        paragraph.textContent = "Dados cidade #" + (indice + 1) + ": ";
-        nome.textContent = dado.nome_cidade;
-        estado.textContent = dado.nome_estado;
+    var nome = document.createElement("h3");
+    var estado = document.createElement("h4");
 
-        div.setAttribute('class', 'city_card')
+    paragraph.textContent = "Dados cidade #" + (indice + 1) + ": ";
+    nome.textContent = dado.nome_cidade;
+    estado.textContent = dado.nome_estado;
 
-        div.appendChild(paragraph);
-        div.appendChild(nome);
-        div.appendChild(estado);
+    div.setAttribute("class", "city_card");
 
-        link.appendChild(div);
-        data_view.appendChild(link);
-    });
+    div.appendChild(paragraph);
+    div.appendChild(nome);
+    div.appendChild(estado);
+
+    link.appendChild(div);
+    data_view.appendChild(link);
+  });
 }
 
+function listarEstados(data) {
+  var data_view = document.getElementById("data");
+  data_view.innerHTML = ""; // Limpar o conteúdo antes de começar
+
+  data[0].forEach(function (dado, indice) {
+    var link = document.createElement("a");
+
+    link.setAttribute("href", `/estado/${dado.id}`);
+
+    var div = document.createElement("div");
+    var paragraph = document.createElement("p");
+
+    var nome = document.createElement("h3");
+    var sigla = document.createElement("h4");
+
+    paragraph.textContent = "Dados estado #" + (indice + 1) + ": ";
+    nome.textContent = dado.nome_estado;
+    sigla.textContent = dado.sigla;
+
+    div.setAttribute("class", "state_card");
+
+    div.appendChild(paragraph);
+    div.appendChild(nome);
+    div.appendChild(sigla);
+
+    link.appendChild(div);
+    data_view.appendChild(link);
+  });
+}
+
+function listarEquipes(data) {
+  var data_view = document.getElementById("data");
+  data_view.innerHTML = ""; // Limpar o conteúdo antes de começar
+
+  data[0].forEach(function (dado, indice) {
+    var link = document.createElement("a");
+
+    link.setAttribute("href", `/equipe/${dado.id}`);
+
+    var div = document.createElement("div");
+    var paragraph = document.createElement("p");
+
+    var nome = document.createElement("h3");
+    var obs = document.createElement("h4");
+    var jogadores = document.createElement("p");
+
+    paragraph.textContent = "Dados equipe #" + (indice + 1) + ": ";
+    nome.textContent = dado.nome_equipe;
+    obs.textContent = dado.observacoes;
+    jogadores.textContent = "Jogadores na equipe:" + dado.numero_jogadores;
+
+    div.setAttribute("class", "team_card");
+
+    div.appendChild(paragraph);
+    div.appendChild(nome);
+    div.appendChild(obs);
+    div.appendChild(jogadores);
+
+    link.appendChild(div);
+    data_view.appendChild(link);
+  });
+}
